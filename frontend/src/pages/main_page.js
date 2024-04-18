c1a74382cb75d2d9a327324981d988b354e98b39
Chawanakon0091chaonai <6422770246@g.siit.tu.ac.th>
2024-04-18 20:13:29 +0700

    test

--- frontend/src/pages/main_page.js
+++ frontend/src/pages/main_page.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Main = () => {
+    const [email, setEmail] = useState('');
+    const [pwd, setPwd] = useState('');
+
+    const handleSubmit = async(e) => {
+    e.preventDefault();
+  
+  try {
+    
+    console.log('response.data'); 
+  
+  } catch (error) {
+    console.error('Error:', error);
+    
+  }
+  }
   return (
-    <div>main_page</div>
+    <>
+        <h1>main_page</h1>
+        <form onSubmit={handleSubmit}>
+            <label>Upload</label>
+            <input type='file' id="file" name="file"></input>
+            <br/>
+            <br/>
+            <input type="submit"></input>
+        </form>
+    </>
   )
 }
 
